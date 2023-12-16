c28782eecb43453c4b0478a42a4bcab5b037f40e
musicshio <shiohiro846@gmail.com>
2023-12-16 21:31:43 +0900

    mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import {Button} from "@mui/material";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <Button variant="contained">Hello world</Button>;
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
