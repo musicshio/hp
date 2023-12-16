d53363ca4da3daf80189ac9d0f1918c2cee15b41
musicshio <shiohiro846@gmail.com>
2023-12-16 22:49:38 +0900

    create sections

---
+++ src/app/(main)/sections/music/MusicSection.tsx
@@ -0,0 +1,9 @@
+import {Typography} from "@mui/material";
+import {SectionBox} from "@/app/(main)/sections/SectionBox";
+
+export const MusicSection = () => {
+    return <SectionBox>
+        <Typography>Music</Typography>
+        <Typography>musics</Typography>
+    </SectionBox>
+}
