f88b53f63324adcbdcc6a4318fc69015175a2a8c
musicshio <shiohiro846@gmail.com>
2024-12-19 23:28:18 +0900

    feat: make path
    close #10

--- src/app/(main)/music/page.tsx
+++ src/app/(main)/music/page.tsx
@@ -8,42 +8,31 @@ import {
   IconButton,
   Link,
   Typography,
-} from '@mui/material'
-import { Section } from '@/app/(main)/sections/Section'
-import { FiExternalLink } from 'react-icons/fi'
-import { PiSoundcloudLogoFill, PiYoutubeLogoFill } from 'react-icons/pi'
+} from "@mui/material";
+import { FiExternalLink } from "react-icons/fi";
+import { PiSoundcloudLogoFill, PiYoutubeLogoFill } from "react-icons/pi";
 
-export const MusicCompositionSection = () => {
+export default function Page() {
   return (
-    <Section>
+    <>
       <Typography variant="h1" gutterBottom>
         Music - Composition
       </Typography>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-      >
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         <Grid item xs={4}>
           <Card>
             <CardHeader title="ヒトと人間" subheader="2023" />
             <CardMedia
               height="250"
-              component={'iframe'}
-              src={
-                'https://www.youtube.com/embed/jE6Blb8wf5U?si=wlhpeUzNm48U2Gob'
-              }
+              component={"iframe"}
+              src={"https://www.youtube.com/embed/jE6Blb8wf5U?si=wlhpeUzNm48U2Gob"}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             />
             <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                <Link href={'https://noas-sandbox.tumblr.com/'} target="_blank">
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                <Link href={"https://noas-sandbox.tumblr.com/"} target="_blank">
                   Hical Records 『ノアの箱庭 -Before The End SandboX』
                   <FiExternalLink />
                 </Link>
@@ -63,14 +52,14 @@ export const MusicCompositionSection = () => {
               <IconButton
                 aria-label="YouTube"
                 target="_blank"
-                href={'https://youtu.be/jE6Blb8wf5U?si=ynbH0TSUu8L-FLa1'}
+                href={"https://youtu.be/jE6Blb8wf5U?si=ynbH0TSUu8L-FLa1"}
               >
                 <PiYoutubeLogoFill />
               </IconButton>
               <IconButton
                 aria-label="SoundCloud"
                 target="_blank"
-                href={'https://soundcloud.com/music_shio/human'}
+                href={"https://soundcloud.com/music_shio/human"}
               >
                 <PiSoundcloudLogoFill />
               </IconButton>
@@ -82,21 +71,15 @@ export const MusicCompositionSection = () => {
             <CardHeader title="ヒトと人間" subheader="2023" />
             <CardMedia
               height="250"
-              component={'iframe'}
-              src={
-                'https://www.youtube.com/embed/jE6Blb8wf5U?si=wlhpeUzNm48U2Gob'
-              }
+              component={"iframe"}
+              src={"https://www.youtube.com/embed/jE6Blb8wf5U?si=wlhpeUzNm48U2Gob"}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             />
             <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                <Link href={'https://noas-sandbox.tumblr.com/'} target="_blank">
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                <Link href={"https://noas-sandbox.tumblr.com/"} target="_blank">
                   Hical Records 『ノアの箱庭 -Before The End SandboX』
                   <FiExternalLink />
                 </Link>
@@ -116,14 +99,14 @@ export const MusicCompositionSection = () => {
               <IconButton
                 aria-label="YouTube"
                 target="_blank"
-                href={'https://youtu.be/jE6Blb8wf5U?si=ynbH0TSUu8L-FLa1'}
+                href={"https://youtu.be/jE6Blb8wf5U?si=ynbH0TSUu8L-FLa1"}
               >
                 <PiYoutubeLogoFill />
               </IconButton>
               <IconButton
                 aria-label="SoundCloud"
                 target="_blank"
-                href={'https://soundcloud.com/music_shio/human'}
+                href={"https://soundcloud.com/music_shio/human"}
               >
                 <PiSoundcloudLogoFill />
               </IconButton>
@@ -131,6 +114,6 @@ export const MusicCompositionSection = () => {
           </Card>
         </Grid>
       </Grid>
-    </Section>
-  )
+    </>
+  );
 }
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,47 +1,15 @@
-'use client'
-import * as React from 'react'
-import Box from '@mui/material/Box'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import { Keyboard, Mousewheel, Navigation, Pagination } from 'swiper/modules'
-import 'swiper/css/pagination'
-import { IntroductionSection } from '@/app/(main)/sections/Introduction/IntroductionSection'
-import { MusicCompositionSection } from '@/app/(main)/sections/music/MusicCompositionSection'
-import { AcapellaSection } from '@/app/(main)/sections/acapella/AcapellaSection'
-import { DesignSection } from '@/app/(main)/sections/design/DesignSection'
-import { EngineeringSection } from '@/app/(main)/sections/engineering/EngineeringSection'
+import * as React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Typography } from "@mui/material";
 
-export default function MainPage() {
+export default function Page() {
   return (
-    <Box width={'100vw'} height={'100vh'}>
-      <Swiper
-        direction={'vertical'}
-        keyboard={{
-          enabled: true,
-        }}
-        mousewheel={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Mousewheel, Keyboard, Pagination, Navigation]}
-        className="main-swiper"
-      >
-        <SwiperSlide>
-          <IntroductionSection />
-        </SwiperSlide>
-        <SwiperSlide>
-          <EngineeringSection />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MusicCompositionSection />
-        </SwiperSlide>
-        <SwiperSlide>
-          <AcapellaSection />
-        </SwiperSlide>
-        <SwiperSlide>
-          <DesignSection />
-        </SwiperSlide>
-      </Swiper>
-    </Box>
-  )
+    <>
+      <Typography variant="h1" gutterBottom>
+        Introduction
+      </Typography>
+      <Typography>musicshio</Typography>
+    </>
+  );
 }
