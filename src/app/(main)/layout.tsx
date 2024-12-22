5bc61dae12bffa9beac0deb63ce70d5973d7573e
musicshio <shiohiro846@gmail.com>
2024-12-22 17:23:16 +0900

    feat: make sidebar responsive
    close #18

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,20 +1,6 @@
 import * as React from "react";
-import { Stack } from "@mui/system";
-import Nav from "@/components/Nav";
+import MainLayout from "@/components/Layout";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <Stack
-      direction={"row"}
-      spacing={2}
-      height={"100dvh"}
-      width={"100dvw"}
-      p={1}
-      boxSizing={"border-box"}
-      sx={{ overflow: "hidden" }}
-    >
-      <Nav />
-      {children}
-    </Stack>
-  );
+  return <MainLayout>{children}</MainLayout>;
 }
