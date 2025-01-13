867c4bae2015b1d2ff42183ce7ccc4ce1f39c321
musicshio <shiohiro846@gmail.com>
2025-01-13 23:56:51 +0900

    feat: add music detail page
    close #26

--- src/app/(main)/music/page.tsx
+++ src/app/(main)/music/page.tsx
@@ -9,42 +9,26 @@ import {
   Typography,
 } from "@mui/material";
 import { Box, Stack } from "@mui/system";
-import Link from "next/link";
 import Image from "next/image";
 import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
-
-const itemData: ItemProps[] = [
-  {
-    id: "tokyo-shandy-randevouz",
-    img: "/images/tokyo-shandy-randevouz.png",
-    externalUrl: "https://youtu.be/7qyZWwgqGf8?si=kOACD8U1H8ucBX0g",
-    title: "トウキョウ・シャンディ・ランデヴ feat. 花譜, ツミキ - MAISONdes",
-    type: "cover",
-    year: 2023,
-  },
-  {
-    id: "gogo",
-    img: "/images/gogo.jpg",
-    externalUrl: "https://youtu.be/uYJMmYAD45w?si=o5Yt1K9h7JzUou0Z",
-    title: "ゴーゴー幽霊船 - 米津玄師",
-    type: "cover",
-    year: 2021,
-  },
-  {
-    id: "human",
-    img: "/images/human.jpg",
-    externalUrl: "https://youtu.be/jE6Blb8wf5U?si=wlhpeUzNm48U2Gob",
-    title: "ヒトと人間",
-    type: "original",
-    year: 2023,
-  },
-];
+import { musicData } from "@/app/(main)/music/_data/data";
+import { Link } from "next-view-transitions";
 
 export default function Page() {
+  const itemData = musicData.map(({ img, id, youtubeId, title, type, year }) => ({
+    img,
+    id,
+    externalUrl: `https://www.youtube.com/watch?v=${youtubeId}`,
+    title,
+    type: type as "original" | "cover",
+    year,
+  }));
   return (
-    <Stack width={"100%"}>
-      <Typography variant="h1">Music</Typography>
-      <Typography variant="subtitle1"></Typography>
+    <Stack width={"100%"} spacing={2}>
+      <Stack>
+        <Typography variant="h1">Music</Typography>
+        <Typography variant="subtitle1">オリジナルが少しと、アカペラカバーがあります。</Typography>
+      </Stack>
       <Divider />
       <Grid2 container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 4, md: 8 }}>
         {itemData.map((item) => (
@@ -73,6 +57,7 @@ function Item({ img, id, title, externalUrl, type, year }: ItemProps) {
         position={"relative"}
         sx={{
           borderRadius: 4,
+          viewTransitionName: `music-card-${id}`,
         }}
       >
         <CardActionArea
@@ -93,6 +78,7 @@ function Item({ img, id, title, externalUrl, type, year }: ItemProps) {
                   transform: "scale(1.02)",
                 },
               },
+              viewTransitionName: `music-content-${id}`,
             }}
           >
             <Image src={img} fill alt={title} objectFit="cover" />
@@ -110,7 +96,14 @@ function Item({ img, id, title, externalUrl, type, year }: ItemProps) {
             <Typography variant={"caption"} color={"gray"}>
               {year}
             </Typography>
-            <Typography color={"white"}>{title}</Typography>
+            <Typography
+              color={"white"}
+              sx={{
+                viewTransitionName: `music-title-${id}`,
+              }}
+            >
+              {title}
+            </Typography>
             <Box>
               <Chip label={type} variant="outlined" />
             </Box>
