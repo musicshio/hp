1f9f17f86d01398b30c7a3275786a02df354c489
musicshio <shiohiro846@gmail.com>
2025-01-13 16:27:47 +0900

    feat: introduce view-transition-api
    close #24

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,31 +5,31 @@ import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import DesignServicesIcon from "@mui/icons-material/DesignServices";
 import CodeIcon from "@mui/icons-material/Code";
 import List from "@mui/material/List";
-import NextLink from "next/link";
 import ModeSwitcher from "./ModeSwitcher";
+import { Link } from "next-view-transitions";
 
 export default function Nav() {
   return (
     <List component="nav" aria-labelledby="navbar">
-      <ListItemButton component={NextLink} href={"/"}>
+      <ListItemButton component={Link} href={"/"}>
         <ListItemIcon>
           <HomeIcon />
         </ListItemIcon>
         <ListItemText primary="Home" />
       </ListItemButton>
-      <ListItemButton component={NextLink} href={"/design"}>
+      <ListItemButton component={Link} href={"/design"}>
         <ListItemIcon>
           <DesignServicesIcon />
         </ListItemIcon>
         <ListItemText primary="Design" />
       </ListItemButton>
-      <ListItemButton component={NextLink} href={"/music"}>
+      <ListItemButton component={Link} href={"/music"}>
         <ListItemIcon>
           <LibraryMusicIcon />
         </ListItemIcon>
         <ListItemText primary="Music" />
       </ListItemButton>
-      <ListItemButton component={NextLink} href={"/code"}>
+      <ListItemButton component={Link} href={"/code"}>
         <ListItemIcon>
           <CodeIcon />
         </ListItemIcon>
