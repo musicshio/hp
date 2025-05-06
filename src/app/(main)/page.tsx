8367364a72dcf4e4a79de06d4e3f6b004f2b1a79
musicshio <shiohiro846@gmail.com>
2025-05-06 18:53:53 +0900

    feat: clean top
    close #58

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,33 +1,24 @@
 import * as React from "react";
-import { Button, ButtonGroup, Typography } from "@mui/material";
-import { Stack } from "@mui/system";
-import Link from "next/link";
+import { Box, Stack, Typography } from "@mui/material";
 
 export default function Page() {
   return (
-    <Stack alignItems={"center"} pt={16} spacing={20}>
-      <Typography variant={"h4"} letterSpacing={4}>
-        文脈の波にかき消されそうな実在の温かさにスポットライトを。
-      </Typography>
-      <Typography>music_shio</Typography>
-      <ButtonGroup
-        variant="text"
-        aria-label="text button group"
-        sx={{
-          "& > *": {
-            border: "none",
-            color: "text.secondary",
-            fontSize: "1.5rem",
-          },
-        }}
-      >
-        <Button component={Link} href="instances">
-          Instances
-        </Button>
-        <Button component={Link} href="fragments">
-          Fragments
-        </Button>
-      </ButtonGroup>
-    </Stack>
+    <Box
+      position={"fixed"}
+      width={"100dvw"}
+      height={"100dvh"}
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      top={0}
+      zIndex={-1}
+    >
+      <Stack alignItems={"center"}>
+        <Typography variant={"subtitle1"}>music_shio</Typography>
+        <Typography variant={"caption"}>
+          文脈の波にかき消されそうな実在の温かさにスポットライトを。
+        </Typography>
+      </Stack>
+    </Box>
   );
 }
